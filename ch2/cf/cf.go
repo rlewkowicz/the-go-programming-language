e391863bdfb8cf39a2efc572723481ad363dbb62
ryan.lewkowicz@ge.com <ryan.lewkowicz@ge.com>
2018-01-08 07:51:59 -0600

    rable rable rable

---
+++ ch2/cf/cf.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/rlewkowicz/the-go-programming-language/ch2/tempconv"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		}
+		f := tempconv.Fahrenheit(t)
+		c := tempconv.Celsius(t)
+		k := tempconv.Kelvin(t)
+	}
+}
