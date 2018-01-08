271cfd717f1bd9f0c9928b82caba1745db5016a8
ryan.lewkowicz@ge.com <ryan.lewkowicz@ge.com>
2018-01-08 05:59:46 -0600

    Begrudgenly doing some book work

--- ch2/2.1/tempconv.go
+++ ch2/2.1/tempconv.go
@@ -10,7 +10,11 @@ const (
 	AbsoluteZeroC Celsius = -273.15
 	FreezingC     Celsius = 0
 	BoilingC      Celsius = 100
+	AbsoluteZeroK Kelvin  = 0
+	FreezingK     Kelvin  = 273.15
+	BoilingK      Kelvin  = 373.15
 )
 
 func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
+func (k Kelvin) String() string     { return fmt.Sprintf("%g°K", k) }
