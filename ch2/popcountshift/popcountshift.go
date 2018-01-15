6909ff365bcd9b5d2d49e2ab5737b6d0027da32c
ryan.lewkowicz@ge.com <ryan.lewkowicz@ge.com>
2018-01-15 13:04:01 -0600

    doot doot

--- ch2/popcountshift/popcountshift.go
+++ ch2/popcountshift/popcountshift.go
@@ -1,11 +1,14 @@
-package popcountshift
+package main
+
+func main() {
+	PopCount(5)
+}
 
 func PopCount(x uint64) int {
 	a := 0
-	for i := uint(1); i <= 64; i++ {
-		if x>>i == 1 {
-			a++
-		}
+	for i := byte(0); i <= 64; i++ {
+		a = +int(x & 1)
+		x >>= 1
 	}
-	return int(a)
+	return a
 }
